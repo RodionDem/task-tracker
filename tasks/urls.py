17c1abe1d12d7c24adecc017af62f85fc8e09ce7
Rodion <r.demianetss@gmail.com>
2026-03-13 15:12:11 +0200

    Add task creation button and improve task list template

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", views.TaskCreateView.as_view(), name="task-create"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 from .models import Task
 
 
@@ -6,3 +7,10 @@ class TaskListView(ListView):
     model = Task
     template_name = "tasks/task_list.html"
     context_object_name = "task_list"
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = ["content", "deadline", "tags"]
+    template_name = "tasks/task_form.html"
+    success_url = reverse_lazy("task-list")
